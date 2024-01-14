6fb1bf8b857e0ef2ae7bf2a097555585c9640b69
Yu Hamada <yuham1014@gmail.com>
2024-01-14 08:36:40 -0800

    change the way to display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Navbar from '@/components/layout/Navbar'
 import Weekly from '@/components/manga/Weekly'
 import CategoryButton from '@/components/manga/CategoryButton'
-import MangaCard from '@/components/manga/MangaCard'
+import { MangaCard } from '@/components/manga/MangaCard'
+
+import { mangas }  from '@/data/manga'
 
 export default function Home() {
   return (
@@ -10,15 +12,13 @@ export default function Home() {
       <main className="py-24">
         <Weekly />
         <CategoryButton />
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-8 gap-x-2">
+          {mangas.map((manga) => (
+            <MangaCard
+              key={manga.id}
+              manga={manga}
+            />
+          ))}
         </div>
       </main>
     </>
