078a9975dcb8c27a575d28c8b4acb26bcd777614
Yu Hamada <yuham1014@gmail.com>
2024-01-14 08:34:14 -0800

    create search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,25 @@
 import Navbar from '@/components/layout/Navbar'
-import Weekly from '@/components/manga/Weekly'
-import CategoryButton from '@/components/manga/CategoryButton'
-import MangaCard from '@/components/manga/MangaCard'
+import { MangaCard } from '@/components/manga/MangaCard'
 import ItemFilter from '@/components/filter/ItemFilter'
 
-export default function Search() {
+import { GetAllMangasParams, SearchParamProps } from '@/types';
+import { getAllMangas } from '@/lib/actions/manga.action';
+
+
+export default async function Search({ searchParams }: SearchParamProps) {
+  const keywordText = (searchParams?.query as string) || '';
+  const genre = (searchParams?.genre as string) || '';
+  const category = (searchParams?.category as string) || '';
+  const sort = (searchParams?.sort as string) || '';
+
+
+  const mangas = await getAllMangas({
+    query: keywordText,
+    genre,
+    category,
+    sort,
+  })
+  
   return (
     <>
       <Navbar />
@@ -12,14 +27,13 @@ export default function Search() {
         <div>
           <ItemFilter />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-8 gap-x-2">
+          {mangas.map((manga) => (
+            <MangaCard
+              key={manga.id}
+              manga={manga}
+            />
+          ))}
         </div>
       </main>
     </>
