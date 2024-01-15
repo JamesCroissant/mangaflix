ea5f5da238bcbd5014d8237c06916e951b66ba95
Yu Hamada <yuham1014@gmail.com>
2024-01-15 15:56:40 -0800

    create button component in home page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        orange: "text-black-500 hover:text-white hover:bg-orange-500 rounded-full"
+        stable: "text-black-500 rounded-full",
       },
       size: {
         default: "h-10 px-4 py-2",
