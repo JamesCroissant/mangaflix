88e58b9ad6b7985588e2b257303cc8ef7bf9026f
Yu Hamada <yuham1014@gmail.com>
2024-01-15 15:59:11 -0800

    add component to display filtering mangas in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,44 @@
 import Navbar from '@/components/layout/Navbar'
-import Weekly from '@/components/manga/Weekly'
+import { Weekly } from '@/components/manga/Weekly'
 import CategoryButton from '@/components/manga/CategoryButton'
-import MangaCard from '@/components/manga/MangaCard'
+import { MangaCard } from '@/components/manga/MangaCard'
 
-export default function Home() {
+import { GetAllMangasParams, SearchParamProps } from '@/types';
+import { getAllMangas } from '@/lib/actions/manga.action';
+
+import { mangas } from '@/data/manga'
+
+
+export default async function Home({ searchParams }: SearchParamProps) {
+  const keywordText = (searchParams?.query as string) || '';
+  const genre = (searchParams?.genre as string) || '';
+  const category = (searchParams?.category as string) || '';
+  const sort = (searchParams?.sort as string) || '';
+
+  const trendingMangas = mangas.filter(manga => manga.category === "trending");
+
+  const filterdMangas = await getAllMangas({
+    query: keywordText,
+    genre,
+    category,
+    sort,
+  })
+
   return (
     <>
       <Navbar />
       <main className="py-24">
-        <Weekly />
+        <Weekly 
+          mangas={trendingMangas}
+        />
         <CategoryButton />
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-          <MangaCard />
-
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-8 gap-x-2">
+          {filterdMangas.map((filterdManga) => (
+            <MangaCard
+              key={filterdManga.id}
+              manga={filterdManga}
+            />
+          ))}
         </div>
       </main>
     </>
--- src/components/manga/MangaCard.tsx
+++ src/components/manga/MangaCard.tsx
@@ -1,24 +1,29 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
 
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { Badge } from '@/components/ui/badge';
-
 import { FaUser } from "react-icons/fa6";
 
-const MangaCard = () => {
+import { Manga } from '@/data/manga';
+
+
+type MangaCardProps = {
+  manga: Manga;
+}
+
+export function MangaCard({ manga }: MangaCardProps) {
+
   return (
     <div className="flex justify-center gap-4">
-      <Card className='flex flex-row justify-center w-[400px] rounded-xl'>
+      <Card className='flex flex-row justify-center w-[400px] rounded-xl transition duration-300 hover:-translate-y-1 hover:scale-105'>
         <CardHeader className='h-40 w-40'>
           <Image
             width={640}
             height={427}
-            src={'/images/default-image.jpeg'}
-            alt='Recipe Image'
+            src={ manga.imgPath || '/images/default-image.jpeg'}
+            alt='Manga Image'
             className='object-cover h-full w-full group-hover:scale-110 transition rounded-2xl'
           />
         </CardHeader>
@@ -26,26 +31,26 @@ const MangaCard = () => {
         <CardContent className="p-0 flex items-center">
           <div>
             <div className="font-semibold text-lg py-1">
-              One Piece
+              { manga.title }
             </div>
             <div className="flex gap-x-3">
-              <div className="flex items-center text-gray-500 gap-x-1.5 text-md">
+              <div className="flex items-center text-gray-500 gap-x-1.5 text-sm">
                 <FaUser
-                  className="text-lg"
+                  className="text-md"
                 />
-                Oda Eiichiro
+                { manga.author }
               </div>
-              <div className="py-1 text-gray-500">
-                ★ 4.5
+              <div className="py-1 text-gray-500 text-sm">
+                ★ { manga.rating }
               </div>
             </div>
             
             <div className='mt-4 flex gap-x-2'>
-              <Badge variant='outline' className='font-medium border-none text-orange-500 bg-rose-200'>
-                Boys Comic
+              <Badge variant='outline' className='font-medium border-none text-orange-500 bg-rose-100'>
+              { manga.category }
               </Badge>
-              <Badge variant='outline' className='font-medium border-none text-orange-500 bg-rose-200'>
-                Popular
+              <Badge variant='outline' className='font-medium border-none text-orange-500 bg-rose-100'>
+                { manga.genre }
               </Badge>
             </div>
           </div>
@@ -55,5 +60,3 @@ const MangaCard = () => {
     
   )
 }
-
-export default MangaCard
