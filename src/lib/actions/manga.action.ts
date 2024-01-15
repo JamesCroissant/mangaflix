a6a07af616f334e039b40c949b6d9950ad115b0f
Yu Hamada <yuham1014@gmail.com>
2024-01-15 15:53:24 -0800

    create function of filtering mangas

---
+++ src/lib/actions/manga.action.ts
@@ -0,0 +1,42 @@
+'use server'
+
+import { mangas } from '@/data/manga';
+
+import { GetAllMangasParams } from '@/types'
+
+
+export async function getAllMangas({ query, genre, category, sort }: GetAllMangasParams) {
+  try {
+    let filteredManga = mangas;
+
+    // keyword
+    if (query) {
+      filteredManga = filteredManga.filter(manga => manga.title.toLowerCase().includes(query.toLowerCase()));
+    }
+
+    // genre
+    if (genre) {
+      filteredManga = filteredManga.filter(manga => manga.genre.toLowerCase() === genre);
+    }
+
+    // category
+    if (category === 'popular' || category === 'trending') {
+      filteredManga = filteredManga.filter(manga => manga.category.toLowerCase() === category);
+    } else if (category === 'ranking') {
+      filteredManga = filteredManga.sort((a, b) => b.rating - a.rating);
+    }
+
+    // sorting
+    if (sort === 'ascending') {
+      filteredManga.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sort === 'descending') {
+      filteredManga.sort((a, b) => b.title.localeCompare(a.title));
+    }
+
+    return filteredManga
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
