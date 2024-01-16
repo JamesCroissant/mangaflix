97f4d7af2ca56eb17ea9534b090eeb62f3e934d3
Yu Hamada <yuham1014@gmail.com>
2024-01-15 16:14:20 -0800

    fix navbottombar component

--- src/components/layout/NavBottomBar.tsx
+++ src/components/layout/NavBottomBar.tsx
@@ -1,45 +1,51 @@
+"use client"
+
+import Link from "next/link"
+import classNames from "classnames"
+import { usePathname } from "next/navigation"
+
 import { RiHomeFill } from "react-icons/ri";
 import { FiSearch } from "react-icons/fi";
-import { FaRegBookmark } from "react-icons/fa";
+import { FaBookmark } from "react-icons/fa";
 import { FaUser } from "react-icons/fa6";
 
 const NavBottomBar = () => {
+  const currentPath = usePathname()
+
+  const getLinkClassNames = (path: string) => classNames({
+    "text-orange-500": path === currentPath,
+    "text-zinc-500": path !== currentPath,
+    "hover:text-orange-600 transition-colors": true,
+  })
+
+  const links = [
+    { label: "Home", href: "/", component: <RiHomeFill />},
+    { label: "Search", href: "/search", component: <FiSearch /> },
+    { label: "Favorite", href: "/favorites", component: <FaBookmark /> },
+    { label: "Account", href: "/account", component: <FaUser /> },
+  ]
+
   return (
     <nav className="flex gap-8 sm:gap-20 items-center">
-      <div className="flex flex-col items-center">
-        <div className="">
-          <RiHomeFill />
+      {links.map(link => 
+        <Link
+          key={link.href}
+          className={getLinkClassNames(link.href)}
+          href={link.href}
+        >
+        <div className="flex flex-col items-center">
+          <div className="text-2xl">
+            {link.component}
+          </div>
+          <div className="mt-1">
+            {link.label}
+          </div>
         </div>
-        <div className="text-gray-500 mt-1">
-          Home
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="">
-          <FiSearch />
-        </div>
-        <div className="text-gray-500 mt-1">
-          Search
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="">
-          <FaRegBookmark />
-        </div>
-        <div className="text-gray-500 mt-1">
-          Favorite
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="">
-          <FaUser />
-        </div>
-        <div className="text-gray-500 mt-1">
-          Account
-        </div>
-      </div>
+        </Link>
+      )}
     </nav>
   )
 }
 
 export default NavBottomBar
+
