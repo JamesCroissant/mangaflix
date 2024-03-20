30639d561172e1bb33c49de5ed526a23eb7d5b41
JamesCroissant <yuham1014@gmail.com>
2024-03-19 23:49:09 -0700

    fix: change the schema

--- prisma/migrations/20240320000804_init/migration.sql
+++ prisma/migrations/20240320000804_init/migration.sql
@@ -1,20 +1,43 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `favoriteIds` on the `User` table. All the data in the column will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "Genre" AS ENUM ('BOYS_COMIC', 'GIRLS_COMIC');
 
 -- CreateEnum
-CREATE TYPE "Category" AS ENUM ('SCIENCE_FICTION', 'FANTASY', 'ROMANCE', 'COMEDY', 'HORROR', 'MYSTERY', 'HISTORY', 'ACTION', 'SPORTS', 'OTHERS');
+CREATE TYPE "Category" AS ENUM ('SF', 'FANTASY', 'ROMANCE', 'COMEDY', 'HORROR', 'MYSTERY', 'HISTORY', 'ACTION', 'SPORTS', 'OTHERS');
 
 -- CreateEnum
 CREATE TYPE "Status" AS ENUM ('ONGOING', 'COMPLETED');
 
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "favoriteIds";
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "hashedPassword" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_Token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Manga" (
@@ -25,11 +48,12 @@ CREATE TABLE "Manga" (
     "description" TEXT,
     "link" TEXT,
     "image" TEXT,
+    "comment" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
-    "genres" "Genre"[],
-    "categories" "Category"[],
-    "statuses" "Status"[],
+    "genre" "Genre" NOT NULL,
+    "category" "Category" NOT NULL,
+    "status" "Status" NOT NULL,
 
     CONSTRAINT "Manga_pkey" PRIMARY KEY ("id")
 );
@@ -55,6 +79,15 @@ CREATE TABLE "Review" (
 
     CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Manga" ADD CONSTRAINT "Manga_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
