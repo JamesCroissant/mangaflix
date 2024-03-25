8a28c92cc5b647bf83afc37223d90982bb0caf37
JamesCroissant <yuham1014@gmail.com>
2024-03-24 21:38:03 -0700

    fix: navigation component

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,37 +2,56 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useRouter } from 'next/navigation';
 
 import { FiSearch } from "react-icons/fi";
+import { HiOutlinePencilAlt } from "react-icons/hi";
 import { UserMenu } from '@/components/layout/UserMenu';
 
-import { SafeUser } from "@/types";
+import { SafeUser } from "@/types/user";
+import useSignInModal from "@/hooks/useSignInModal";
 
 
 type navbarProps = {
   currentUser?: SafeUser | null;
 };
 
-const Navbar:React.FC<navbarProps> = ({ currentUser }) => {
-  const currentPath = usePathname()
+const Navbar = ({ currentUser }: navbarProps) => {
+  const router = useRouter();
+  const currentPath = usePathname();
+  const loginModal = useSignInModal();
+
+  const handleClick = () => {
+    if (currentUser) {
+      router.push('/posts/create');
+    } else {
+      loginModal.onOpen();
+    }
+  };
 
   return (
     <>
       <div className="fixed w-full bg-white z-10 shadow-sm">
         <div className="py-3 border-b-[1px] flex justify-evenly">
-          <Link href="/">
+          <Link href="/" className='flex items-center'>
             <h1 className="text-2xl font-semibold text-left text-orange-500">
               Manga Flix
             </h1>
           </Link>
-          <nav className="flex gap-6 items-center">
+          <nav className="flex gap-4 items-center">
             <Link
               key='/search'
               className={`${currentPath === "/search" ? "text-orange-500" : "text-zinc-500"} hover:text-orange-600 transition-colors`}
               href="/search"
             >
-              <FiSearch /> 
+              <FiSearch />
             </Link>
+            <div
+              className={`${currentPath === "/posts/create" ? "text-orange-500" : "text-zinc-500"} hover:text-orange-600 transition-colors`}
+              onClick={handleClick}
+            >
+              <HiOutlinePencilAlt className="text-xl"/>
+            </div>
             <UserMenu currentUser={currentUser} />
           </nav>          
         </div>
