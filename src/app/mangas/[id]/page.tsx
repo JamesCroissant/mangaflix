8f9577a5f694ac0e54d11097a88c22f07b23f267
JamesCroissant <yuham1014@gmail.com>
2024-03-24 22:00:00 -0700

    fix: change mangas detail page

---
+++ src/app/mangas/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { EmptyResult } from '@/components/layout/EmptyResult';
+import Navbar from '@/components/layout/Navbar'
+import { MangaDetailCard } from '@/components/manga/MangaDetailCard'
+
+import { getCurrentUser } from '@/lib/service/getCurrentUser'
+import { getManga } from '@/lib/service/getManga';
+
+export default async function MangaDetail({ 
+  params,
+}: {
+  params: {id: string}
+}) {
+  const manga = await getManga(params.id);
+  const currentUser = await getCurrentUser();
+
+  if (!manga) return <EmptyResult />;
+
+  return (
+    <>
+      <Navbar currentUser={currentUser}/>
+      <main className="py-24">
+        <MangaDetailCard
+          key={manga.id}
+          manga={manga}
+          currentUser={currentUser}
+        />
+      </main>
+    </>
+  )
+}
