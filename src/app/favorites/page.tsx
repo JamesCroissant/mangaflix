b71d071ee445549c92baf53feb62109c6bbf29cc
JamesCroissant <yuham1014@gmail.com>
2024-03-24 21:59:32 -0700

    fix: change the favorites page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -28,7 +28,6 @@ export default async function Favorite() {
     <>
       <Navbar currentUser={currentUser}/>
       <main className="py-24">
-        {/* <h1 className="text-3xl text-center md:text-left font-semibold md:ml-32 mb-10">My Favorites</h1> */}
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-8 gap-x-2">
           {favoriteMangas.map((favoriteManga) => (
             <MangaCard
